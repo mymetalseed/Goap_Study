1d4b628c06b83771f3ac1a471f70f3933705593e
834930269 <834930269@qq.com>
2023-08-08 00:08:22 +0800

    Goal/Action/WorldState/Precondition/Effect

---
+++ Goap_Study/Assets/Scripts/GAgent.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class SubGoal
+{
+    public Dictionary<string, int> sgoals;
+    public bool remove;
+
+    public SubGoal(string s, int i, bool r)
+    {
+        sgoals = new Dictionary<string, int>();
+        sgoals.Add(s,i);
+        remove = r;
+    }
+}
+
+public class GAgent : MonoBehaviour
+{
+    public List<GAction> actions = new List<GAction>();
+    public Dictionary<SubGoal, int> goals = new Dictionary<SubGoal, int>();
+    
+    //todo: 待实现
+    private GPlanner planner;
+    private Queue<GAction> actionQueue;
+    public GAction currentAction;
+    private SubGoal currentGoal;
+
+    private void Start()
+    {
+        GAction[] acts = this.GetComponents<GAction>();
+        foreach (GAction a in acts)
+        {
+            actions.Add(a);
+        }
+    }
+
+    private void LateUpdate()
+    {
+        
+    }
+}
